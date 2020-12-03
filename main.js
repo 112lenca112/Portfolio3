fefeb1fb5c4d071732c4c5207c7bb8cd8ae89a06
112lenca112 <72919798+112lenca112@users.noreply.github.com>
2020-12-03 19:10:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿
+
+require.config({
+    baseUrl: "js",
+    paths : {
+        knockout: "lib/knockout/knockout-latest"
+    }
+});
+
+
+
+
+
+require(['knockout', 'BookmarkviewModel', 'UserviewModel'], function (ko, bookmarkviewModel, userviewModel) {
+    var viewmodel = {
+        bookmarkviewModel, userviewModel
+    }
+   
+    ko.applyBindings(viewmodel);
+
+});
+
+
+
+
